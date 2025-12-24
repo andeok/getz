7c150aa6053978bbaa9966381796cc54c9ef36eb
Deokgyun an <97350241+andeok@users.noreply.github.com>
2025-12-24 22:37:14 +0900

    Feat/getz 7 (#9)
    * feat(인증): JWT 기반 인증 기능 추가(작업중)
    
    - JWT 설정 추가
    - 로그인 및 토큰 검증 API 구현
    - 로그인 멤버 어노테이션 추가
    
    * refactor: TokenProvider 클래스 코드 정리 및 일관성 유지
    Resolves #7

--- backend/src/main/java/kr/getz/personal/auth/controller/AuthController.java
+++ backend/src/main/java/kr/getz/personal/auth/controller/AuthController.java
@@ -2,12 +2,15 @@ package kr.getz.personal.auth.controller;
 
 import kr.getz.personal.auth.dto.request.LoginRequest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import kr.getz.personal.auth.service.AuthService;
+import kr.getz.personal.global.jwt.TokenProvider;
 import kr.getz.personal.member.dto.SignUpRequest;
 import lombok.RequiredArgsConstructor;
 
@@ -17,11 +20,13 @@ import lombok.RequiredArgsConstructor;
 public class AuthController {
 
 	private final AuthService authService;
+	private final TokenProvider tokenProvider;
 
 	@PostMapping("/signup")
 	public ResponseEntity<Long> signUp(@RequestBody SignUpRequest request) {
 		return ResponseEntity.ok(authService.signup(request));
 	}
+
 
 	@PostMapping("/login")
 	public ResponseEntity<?> login(@RequestBody LoginRequest request) {
