9aa4626ba94329e9d67e11f9fe26ff66a37efb56
andeok <micron6594@naver.com>
2025-12-24 15:02:54 +0900

    feat(회원): 회원 클래스에 이메일, 비밀번호, 이름 필드 추가
    - 이메일 필드를 고유하고 필수로 설정
    - 비밀번호와 이름 필드를 필수로 설정
    
    해결: #3

--- backend/src/main/java/kr/getz/personal/member/domain/Member.java
+++ backend/src/main/java/kr/getz/personal/member/domain/Member.java
@@ -1,5 +1,6 @@
 package kr.getz.personal.member.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,8 +19,14 @@ public class Member extends BaseTimeEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@Column(unique = true, nullable = false)
 	private String email;
+
+	@Column(nullable = false)
 	private String password;
+	
+	@Column(nullable = false)
 	private String name;
 
 	@Builder
