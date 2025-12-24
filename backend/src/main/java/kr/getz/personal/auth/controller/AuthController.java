599ca2709d359e26f576312efc1feb410c27d4ed
andeok <micron6594@naver.com>
2025-12-24 16:26:10 +0900

    feat(인증): JWT 기반 인증 기능 추가(작업중)
    - JWT 설정 추가
    - 로그인 및 토큰 검증 API 구현
    - 로그인 멤버 어노테이션 추가

--- backend/src/main/java/kr/getz/personal/auth/controller/AuthController.java
+++ backend/src/main/java/kr/getz/personal/auth/controller/AuthController.java
@@ -1,12 +1,15 @@
 package kr.getz.personal.auth.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import kr.getz.personal.auth.service.AuthService;
+import kr.getz.personal.global.jwt.TokenProvider;
 import kr.getz.personal.member.dto.SignUpRequest;
 import lombok.RequiredArgsConstructor;
 
@@ -16,10 +19,21 @@ import lombok.RequiredArgsConstructor;
 public class AuthController {
 
 	private final AuthService authService;
+	private final TokenProvider tokenProvider;
 
 	@PostMapping("/signup")
 	public ResponseEntity<Long> signUp(@RequestBody SignUpRequest request) {
 		return ResponseEntity.ok(authService.signup(request));
+	}
+
+	@GetMapping
+	public ResponseEntity<?> login() {
+		return ResponseEntity.ok(tokenProvider.createAccessToken(1L, "USER"));
+	}
+
+	@GetMapping("/{token}")
+	public ResponseEntity<?> test(@PathVariable String token) {
+		return ResponseEntity.ok(tokenProvider.validateAccessToken(token));
 	}
 
 }
